733df5b87f02bb3ada2456716cfa801610485240
pmihay85-png <pmihay85@gmail.com>
2025-11-30 21:51:29 +0200

    Methods homework
    2 main tasks for Methods

---
+++ MethodForGreetings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MethodForGreetings
+{
+    static string MagicGreeting(string name)
+    {
+        string[] greetings =
+        {
+            $"Welcome, mighty {name}!",
+            $"Greetings, traveler {name}!",
+            $"Hail, noble {name}!",
+            $"Behold, the legendary {name} has arrived!"
+        };
+
+        Random r = new Random();
+        return greetings[r.Next(greetings.Length)];
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine(MagicGreeting(userName));
+    }
+}
---
+++ MethodForTheBiggestElementInTheArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+//Write a method that finds the biggest element of an array. Use that method to
+//implement sorting in descending order.
+
+class MethodForTheBiggestElementInTheArray
+{
+    static int FindBiggest(int[] array)
+    {
+        int thebiggest = array[0];
+
+        foreach (int num in array)
+        {
+            if (num > thebiggest)
+                thebiggest = num;
+        }
+
+        return thebiggest;
+    }
+
+
+    static void Main()
+    {
+        int[] numbers = { 3, 7, 2, 33, 1, 15, 4 };
+
+        Console.WriteLine("Biggest element: " + FindBiggest(numbers));
+      
+    }
+}
